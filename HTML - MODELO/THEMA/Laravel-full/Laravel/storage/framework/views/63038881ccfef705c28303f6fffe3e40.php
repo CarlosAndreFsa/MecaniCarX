24deb9c666c97b382d030a1b7ea436e91edf9416
Carlos André <155030347+CarlosAndreFsa@users.noreply.github.com>
2026-01-01 23:35:31 -0300

    Delete HTML - MODELO/THEMA directory

--- HTML - MODELO/THEMA/Laravel-full/Laravel/storage/framework/views/63038881ccfef705c28303f6fffe3e40.php
+++
@@ -1,9 +0,0 @@
-<!-- Layout config Js -->
-<script src="<?php echo e(URL::asset('build/js/layout.js')); ?>"></script>
-<?php echo $__env->yieldPushContent('css'); ?>
-<!-- Icons CSS -->
-<link rel="stylesheet" href="<?php echo e(URL::asset('build/css/icons.min.css')); ?>">
-<!-- Tailwind CSS -->
-<link rel="stylesheet" href="<?php echo e(URL::asset('build/css/tailwind.min.css')); ?>">   
-<script>var app_url = '<?php echo e(env('APP_URL')); ?>' </script>
-<?php /**PATH D:\Laravel\resources\views/layouts/head-css.blade.php ENDPATH**/ ?>
