d426ab88f55c9f5807cbbcddf0a350affaecd219
Carlos André <programadorfsaba@gmail.com>
2026-01-12 22:37:52 -0300

    criação index usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $users = User::where('company_id', $request->user()->company_id)
+            ->where('role', '!=', 'client')
+            ->get();
+dd($users);
+            return view('users.index', compact('users'));
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -7,7 +8,7 @@ Route::view('/', 'welcome');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
+Route::get('\users', UserController::class,'index')->name('users.index');
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
