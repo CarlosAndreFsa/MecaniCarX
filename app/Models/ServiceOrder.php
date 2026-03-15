238dae6bebae44d7b4fcad64725742faa77d650f
Carlos André <programadorfsaba@gmail.com>
2026-03-15 13:51:14 -0300

    feat: Integra vehicle_id no Model e Controller da ServiceOrder

--- app/Models/ServiceOrder.php
+++ app/Models/ServiceOrder.php
@@ -11,6 +11,7 @@ class ServiceOrder extends Model
     protected $fillable = [
         'company_id',
         'customer_id',
+        'vehicle_id',
         'number',
         'title',
         'technical_description',
