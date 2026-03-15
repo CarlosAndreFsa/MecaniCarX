74644b79198a310bdc6613d31541ab9227462b53
Carlos André <programadorfsaba@gmail.com>
2026-03-14 23:12:17 -0300

    ajuste na welcome para home e melhorias na view

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,9 @@ use App\Http\Controllers\UserController;
 use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home');
 
-Route::view('/dashboard', 'dashboard')
-->middleware(['auth'])
+Route::middleware(['auth'])->get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
 Route::middleware(['auth', 'company','active'])->get('/painel', [DashboardController::class, 'index'])->name('painel');
